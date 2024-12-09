a0dad9b148da758f896ea1046dde39a50b6e0c18
Hristo Chilov <97700326+Kanyahi@users.noreply.github.com>
2024-12-09 15:07:32 +0200

    Update loginApp.js

--- Модули/loginFormPopUp/loginApp.js
+++ Модули/loginFormPopUp/loginApp.js
@@ -1,17 +1,50 @@
-function popUpLoginBox(){
-   let loginBox = document.getElementById("loginBox");
-   loginBox.style.display = "block"
+function popUpLoginBox() {
+  let loginBox = document.getElementById("loginBox");
+  loginBox.style.display = "block";
 }
 
-function loginAccess(){
-    let inputID = document.getElementById('yourID').value;
-    let check = ['yes', 'христо чилов'];
+function closeBox() {
+  document.getElementById("loginBox").style.display = "none";
+}
 
-    if (check.includes(inputID)) {
-        alert("You are logged in");
-        document.getElementById('yourID').value = "";
-        document.getElementById("loginBox").style.display = "none";
+function loginAccess() {
+ 
+  fetch(
+    "https://docs.google.com/spreadsheets/d/e/2PACX-1vRUnlKvYhZHa7kgtL_apTIMAzlZ6BoNUqb6bXDJY1Z4LWgyPD-4AIUK7naRsqT8wwJLXiJyN9_acVZn/pub?gid=0&single=true&output=csv"
+  )
+    .then((response) => response.text())
+    .then((csvText) => {
+      const arrayFromInput = csvText.split("\n");
+      arrayFromInput.shift().split(",");
+
+      let inputID = document.getElementById("yourID").value.toLocaleLowerCase();
+      let statusBox = document.getElementById("printStatus");
+     
+      let arrayName = Boolean;
+
+      for (let check of arrayFromInput) {
+        let arrayList = check.split(",");
+        let onlyName = arrayList[1].toLocaleLowerCase().trimEnd();
+
+        if(onlyName === inputID){
+            arrayName = true;
+            break;
         } else {
-            alert("Incorrect ID");
-            }
+            arrayName = false;
+        }
+      }
+      
+
+      if (arrayName) {
+        document.getElementById("yourID").value = "";
+        document.getElementById("loginBox").style.display = "none";
+        return (location.href = "/p/blog-page_42.html");
+      } else {
+        statusBox.textContent += "Incorrect ID";
+        document.getElementById("yourID").value = "";
+        setTimeout(() => {
+          statusBox.textContent = "";
+        }, 2000);
+      }
+    });
 }
